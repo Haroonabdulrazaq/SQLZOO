f662df15844f86a8d2896875c8a86e93883f8592
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-07-29 18:10:04 +0100

    Select_name thirtenth commit

--- Select_names.sql
+++ Select_names.sql
@@ -41,7 +41,7 @@
  SELECT name
   FROM world
  WHERE capital LIKE concat(name,' City')
-
+-- Find the capital and the name where the capital includes the name of the country.
  SELECT capital, name
   FROM world
  WHERE capital LIKE name OR capital LIKE concat(name,' City') OR capital LIKE concat(name,'%','%') 
