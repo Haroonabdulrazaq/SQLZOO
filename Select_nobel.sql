d538a6133e35d92152ea6ad4c625f4bf96e4607f
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-07-30 13:24:59 +0100

    Select_nobel fifth solution

--- Select_nobel.sql
+++ Select_nobel.sql
@@ -14,3 +14,7 @@ SELECT yr, subject, winner
  SELECT winner
   FROM nobel
  WHERE subject = 'Peace' AND yr  >= 2000
+
+ SELECT yr, subject, winner
+  FROM nobel
+ WHERE subject = 'Literature' AND yr  BETWEEN 1980 AND 1989
