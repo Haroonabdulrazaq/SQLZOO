eb7955860dd3130b1524379f4b3d44937990af03
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-07-30 14:31:47 +0100

    Select_nobel fourteen solution

--- Select_nobel.sql
+++ Select_nobel.sql
@@ -54,3 +54,13 @@ WHERE winner = 'EUGENE O\'NEILL'
 SELECT winner, yr, subject 
 FROM nobel
 WHERE winner LIKE 'Sir %' ORDER BY yr DESC, winner ASC
+
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY 
+CASE WHEN 
+subject IN ('Physics','Chemistry')
+THEN 1
+ ELSE 0 END,
+    subject,winner
