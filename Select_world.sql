00751e64c5bdacdecf73b34c1bd00ee49b97df2e
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-07-30 11:48:05 +0100

    Select_World eleventh or 13th commit

--- Select_world.sql
+++ Select_world.sql
@@ -34,3 +34,7 @@ WHERE continent ='South America'
 SELECT name, capital
 FROM world
 WHERE LEFT(name,1) LIKE LEFT(capital,1) AND name <> capital
+
+SELECT name
+   FROM world
+WHERE name LIKE '%o%a%i%ue%'
