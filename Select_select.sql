d816f8546bd3b3a25cacc93b7ca680c48250aa09
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-07-31 22:33:23 +0100

    Select_select second commit

--- Select_select.sql
+++ Select_select.sql
@@ -2,4 +2,8 @@ SELECT name FROM world
   WHERE population >
      (SELECT population FROM world
       WHERE name='Romania')
-      
+
+      SELECT name FROM world
+WHERE continent='Europe' AND gdp/population >(
+SELECT gdp/population FROM world
+WHERE name = 'United Kingdo
