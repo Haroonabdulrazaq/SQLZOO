3d23a6ce21faccf3fc266ef67d36ea9fd58af152
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-07 15:28:15 +0100

    More_join solution 11

--- more_join.sql
+++ more_join.sql
@@ -57,3 +57,10 @@ ON actor.id = actorid
 INNER JOIN movie
 ON movie.id = movieid
 WHERE yr = 1962 AND ord =1
+
+SELECT yr,COUNT(title) FROM
+  movie JOIN casting ON movie.id=movieid
+        JOIN actor   ON actorid=actor.id
+WHERE name='Rock Hudson'
+GROUP BY yr
+HAVING COUNT(title) > 2
